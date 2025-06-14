f47e299d0d964d5079f74b7242de81560af0f7cb
LandonLechner <landonl64@gmail.com>
2025-06-13 22:34:59 -0400

    Added number input system with local storage

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
+const edit = document.querySelector('#edit-btn');
+const modal = document.querySelector('#modal');
+const memberNumInput = document.querySelector('#member-num-field');
+const submit = document.querySelector('#submit-btn');
+let memberNum;
+
 const car = document.getElementById('car');
 const spec = document.getElementById('spec');
 
+edit.addEventListener('click', () =>
+    modal.classList.toggle('hidden')
+)
+
+submit.addEventListener('click', () => {
+    modal.classList.toggle('hidden')
+    memberNum = Number(memberNumInput.value)
+    car.innerText = `car_${memberNum}.tga`
+    spec.innerText = `car_spec_${memberNum}.tga`
+    localStorage.setItem("memberNumber", memberNum);
+})
+
+
 car.addEventListener('click', async function() {
     try {
-        await navigator.clipboard.writeText('car_382044.tga');
+        await navigator.clipboard.writeText(`car_${memberNum}.tga`);
         
         car.innerText = "Copied!";
         
         setTimeout(() => {
-            car.innerText = "car_382044.tga";
+            car.innerText = `car_${memberNum}.tga`;
         }, 1300);
 
     } catch (err) {
@@ -18,15 +37,28 @@ car.addEventListener('click', async function() {
 
 spec.addEventListener('click', async function() {
     try {
-        await navigator.clipboard.writeText('car_spec_382044.tga');
+        await navigator.clipboard.writeText(`car_spec_${memberNum}.tga`);
         
         spec.innerText = "Copied!";
         
         setTimeout(() => {
-            spec.innerText = "car_spec_382044.tga";
+            spec.innerText = `car_spec_${memberNum}.tga`;
         }, 1300);
 
     } catch (err) {
         console.error('Failed to copy text: ', err);
     }
 });
+
+function loadPrevMemberNum () {
+    console.log(memberNum)
+    if (memberNum !== null) {
+        memberNum = localStorage.getItem("memberNumber");
+        car.innerText = `car_${memberNum}.tga`
+        spec.innerText = `car_spec_${memberNum}.tga`
+    }
+}
+
+loadPrevMemberNum();
+
+
